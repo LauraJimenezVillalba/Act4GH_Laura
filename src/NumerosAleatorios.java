498cda8b5a12005107de769a11b96323ac4a4958
Laura Jiménez Villalba <a_laura.jimenez.villalba@iespablopicasso.es>
2023-01-20 14:29:30 -0100

    Primeros ficheros

---
+++ src/NumerosAleatorios.java
@@ -0,0 +1,16 @@
+public class NumerosAleatorios {
+
+  public static void main(String[] args) {
+    
+    final int LONGITUD_NUMEROS = 20;
+    final int MAXIMO = 1;
+    final int MINIMO = 10;
+    int[] numerosAleatorios = new int[LONGITUD_NUMEROS];
+    for (int i = 0; i < LONGITUD_NUMEROS; i++) {
+      numerosAleatorios[i] = (int)(Math.random() * (MAXIMO - MINIMO + 1) + MINIMO);
+      System.out.println("numerosAleatorios[" + i + "] = " + numerosAleatorios[i]);
+    }
+
+  }
+
+}
