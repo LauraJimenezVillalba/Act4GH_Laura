9cb9b55bb0a0a9dc07f0f005b2f1bc3758320220
Laura Jiménez Villalba <a_laura.jimenez.villalba@iespablopicasso.es>
2023-01-23 14:31:01 -0100

    Añadido método Print

--- src/NumerosAleatorios.java
+++ src/NumerosAleatorios.java
@@ -12,5 +12,11 @@ public class NumerosAleatorios {
     }
 
   }
+  
+  public void printArray(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+      System.out.println("array[" + i + "] = " + array[i]);
+    }
+  }
 
 }
