83413154bfab2a61c8c17200ffc20a9ff70e6db6
Laura Jiménez Villalba <a_laura.jimenez.villalba@iespablopicasso.es>
2023-01-26 12:10:16 -0100

    Añadido método contador

--- src/NumerosAleatorios.java
+++ src/NumerosAleatorios.java
@@ -18,5 +18,17 @@ public class NumerosAleatorios {
       System.out.println("array[" + i + "] = " + array[i]);
     }
   }
+  
+  public void contarArray(int[] array) {
+    int[] duplicados = new int[array.length];
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == i) {
+          duplicados[i]++;
+      }
+    for (int j = 1; j < (array.length + 1); j++) {
+      System.out.println("El número " + j + " se repite " + duplicados[j] + " veces");
+    }
+  }
+  }
 
 }
